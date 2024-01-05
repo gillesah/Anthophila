b3092e6ebd7dadbf07157f0f11a109f5b954b08c
Gilles Helleu <helleugilles@gmail.com>
2024-01-05 15:17:23 +0100

    change the view core and change the template index

--- Anthophila/anthophila_app/views/core.py
+++ Anthophila/anthophila_app/views/core.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from anthophila_app.models import Beeyard
+from anthophila_app.models import Beeyard, Beehive, Status
 
 
 def index_view(request):
     user_beeyards = Beeyard.objects.filter(beekeeper=request.user)
-    return render(request, 'index.html', {'beeyards': user_beeyards})
+    user_beehives = Beehive.objects.filter(beeyard__in=user_beeyards)
+    beehive_status = Status.objects.filter(beehive__in=user_beehives)
+    return render(request, 'index.html', {'beeyards': user_beeyards, 'beehives': user_beehives, 'status': beehive_status})
--- Anthophila/readme.md
+++ Anthophila/readme.md
@@ -5,11 +5,6 @@ Application concernant l'apiculture ayant les deux objectifs principaux suivants
 - Permettre aux apiculteurs de gérer leur cheptel de ruches
 - Mettre à disposition publiquement les données concernant les ruches et les abeilles
 
-## To do
-
-[] Mise en place de Djoser  
-[] ADMIN : voir inline admin
-
 ## Les beekeeper dans la bdd
 
 Roger  
