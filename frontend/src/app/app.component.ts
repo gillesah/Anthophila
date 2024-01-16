ea7de0d403f9924d3b994727c6bbf16cda84050b
Gilles Helleu <helleugilles@gmail.com>
2024-01-16 12:19:54 +0100

    trying angular again

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommoModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true, // Ajoutez le drapeau standalone: true ici
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  beeyards: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('http://localhost:8000/API_PUBLIC/beeyards/')
+      .subscribe((data) => {
+        this.beeyards = data;
+      });
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,3 @@
-// import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-// export const routes: Routes = [];
-
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BeeyardComponent } from './beeyard/beeyard.component';
-
-export const routes: Routes = [{ path: '', component: BeeyardComponent }];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+export const routes: Routes = [];
