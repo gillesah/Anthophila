8a3bddf55d302e6ab9da29b996f4490cd67e91b1
Gilles Helleu <helleugilles@gmail.com>
2024-01-22 14:41:24 +0100

    change the dockerfile in Angular. It's working !

--- frontend2/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpHandler } from '@angular/common/http';
-
-@NgModule({
-  imports: [BrowserModule, HttpClientModule],
-  providers: [HttpHandler],
-})
-export class AppModule {}
--- frontend2/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
